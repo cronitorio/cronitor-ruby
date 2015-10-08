8b9d81e4c58953a4b579c176a7c89c9dc318dc02
Jeff Byrnes <jeff@evertrue.com>
2015-10-08 15:25:36 -0400

    Prefer let to a before :context group
    Allows us to set `monitor` once, and lazily evaluate it while changing
    the values of the arguments passed to it.
    
    DRY it up

--- spec/cronitor_spec.rb
+++ spec/cronitor_spec.rb
@@ -1,28 +1,27 @@
 require 'spec_helper'
 
 RSpec.describe Cronitor do
+  let(:monitor) { Cronitor.new token: token, opts: monitor_options, code: code }
+
   it 'has a version number' do
     expect(Cronitor::VERSION).not_to be nil
   end
 
   context 'sets its config correctly' do
-    before(:context) do
-      @monitor = Cronitor.new(
-        token: '1234',
-        opts: { name: 'My Fancy Monitor' },
-        code: 'abcd')
-    end
+    let(:token) { '1234' }
+    let(:monitor_options) { { name: 'My Fancy Monitor' } }
+    let(:code) { 'abcd' }
 
     it 'has the specified API token' do
-      expect(@monitor.token).to eq '1234'
+      expect(monitor.token).to eq '1234'
     end
 
     it 'has the specified options' do
-      expect(@monitor.opts[:name]).to eq 'My Fancy Monitor'
+      expect(monitor.opts[:name]).to eq 'My Fancy Monitor'
     end
 
     it 'has the specified code' do
-      expect(@monitor.code).to eq 'abcd'
+      expect(monitor.code).to eq 'abcd'
     end
   end
 
