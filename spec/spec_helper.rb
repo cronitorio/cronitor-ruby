9385c898c5b6eea6595c5d9a8f2274fc9ab3dc2d
Jeff Byrnes <jeff@evertrue.com>
2015-10-13 12:20:55 -0400

    Drop RSpec warnings due to noise

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,6 @@ RSpec.configure do |config|
 
   config.example_status_persistence_file_path = 'spec/examples.txt'
   config.disable_monkey_patching!
-  config.warnings = true
 
   config.default_formatter = 'doc' if config.files_to_run.one?
   config.order = :random
